84923e311a8264087922cec7625a514c86069911
Miguel <welding3000@users.noreply.github.com>
2017-01-29 22:30:22 -0500

    Create mdd-structure-table.js

---
+++ Md-table/mdd-structure-table.js
@@ -0,0 +1,482 @@
+<script>
+
+var i={
+
+h:'holaaaaaaaaaa',  
+h1:'hola',      
+
+
+/*  
+Marca
+Modelo
+Generación
+Motor
+Numero de puertas
+Potencia máxima
+Velocidad máxima
+Aceleración 0 - 100 km/h
+Capacidad depósito
+Año de la puesta en producción
+tipo Coupe
+Numero de plazas
+Longitud
+Anchura
+Altura
+Distancia entre ejes
+Vía delantera
+Vía trasera
+Capacidad maletero min.
+Capacidad maletero max.
+Posición del motor
+Cilindrada -real-
+Par máximo
+Sistema de combustible
+Distribución
+Construcción de los cilindros
+Número Cilindros
+Diámetro del cilindro
+Recorrido del cilindro
+Ratio de compresión
+Número de válvulas por cilindro
+Combustible
+Drive
+Número de engranaje (transmisión automática )
+Suspensión delantera
+Suspensión trasera
+Frenos delanteros
+Frenos traseros
+ABS
+Dirección, tipo
+Dirección asistida
+consumo de combustible urbano
+consumo de combustible extraurbano
+Peso en orden de marcha
+Peso máximo admisible
+Tamaño de neumáticos
+*/
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+d      :'  	',   d1     :'  	',
+d2     :'  	',   d3     :'  	',
+d4     :'  	',   d5     :'  	',
+d6     :'  	',   d7     :'  	',
+d8     :'  	',   d9     :'  	',
+d10    :'  	',   d11    :'  	',
+d12    :'  	',   d13    :'  	',
+d14    :'  	',   d15    :'  	',
+d16    :'  	',   d17    :'  	',
+d18    :'  	',   d19    :'  	',
+d20    :'  	',   d21    :'  	',
+d22    :'  	',   d23    :'  	',
+d24    :'  	',   d25    :'  	',
+d26    :'  	',   d27    :'  	',
+d28    :'  	',   d29    :'  	',
+d30    :'  	',   d31    :'  	',
+d32    :'  	',   d33    :'  	',
+d34    :'  	',   d35    :'  	',
+d36    :'  	',   d37    :'  	',
+d38    :'  	',   d39    :'  	',
+d40    :'  	',   d41    :'  	',
+d42    :'  	',   d43    :'  	',
+d44    :'  	',   d45    :'  	',
+d46    :'  	',   d47    :'  	',
+d48    :'  	',   d49    :'  	',
+d50    :'  	',   d51    :'  	',
+d52    :'  	',   d53    :'  	',
+d54    :'  	',   d55    :'  	',
+d56    :'  	',   d57    :'  	',
+d58    :'  	',   d59    :'  	',
+d60    :'  	',   d61    :'  	',
+d62    :'  	',   d63    :'  	',
+d64    :'  	',   d65    :'  	',
+d66    :'  	',   d67    :'  	',
+d68    :'  	',   d69    :'  	',
+d70    :'  	',   d71    :'  	',
+d72    :'  	',   d73    :'  	',
+d74    :'  	',   d75    :'  	',
+d76    :'  	',   d77    :'  	',
+d78    :'  	',   d79    :'  	',
+d80    :'  	',   d81    :'  	',
+d82    :'  	',   d83    :'  	',
+d84    :'  	',   d85    :'  	',
+d86    :'  	',   d87    :'  	',
+d88    :'  	',   d89    :'  	',
+d90    :'  	',   d91    :'  	',
+d92    :'  	',   d93    :'  	',
+d94    :'  	',   d95    :'  	',
+d96    :'  	',   d97    :'  	',
+d98    :'  	',   d99    :'  	',
+d100   :'  	',   d101   :'  	',
+d102   :'  	',   d103   :'  	',
+d104   :'  	',   d105   :'  	',
+d106   :'  	',   d107   :'  	',
+d108   :'  	',   d109   :'  	',
+d110   :'  	',   d111   :'  	',
+d112   :'  	',   d113   :'  	',
+d114   :'  	',   d115   :'  	',
+d116   :'  	',   d117   :'  	',
+d118   :'  	',   d119   :'  	',
+d120   :'  	',   d121   :'  	',
+  
+  
+  
+  
+}
+
+</script>
+
+<style>
+table {   width:100%;}
+table, th, td {
+    border: 3px solid aqua;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 10px;
+    text-align: left;
+}
+table tr:nth-child(even) {  background-color: #eee;}
+
+</style>
+
+
+
+var t={
+// start  
+tb:'<table>',
+tr:'<tr>',
+td:'<td>',  
+th:'<th>',
+// end  
+endtb:'</table>',  
+endtr:'</tr>',
+endtd:'</td> ',  
+endth:'</th>',
+}
+// begin - end table
+var at= t.tb ;  var at1=t.endtb ;
+// begin - end th
+var th= t.th;   var th1= t.endth;
+// begin - end tr
+var tr = t.tr;  var tr1= t.endtr;
+// begin - end td
+var td = t.td;  var td1= t.endtd;
+
+document.write(
+// begin   
+at
+   +tr
+       +th+  i.h  +th1 
+       +th+  i.h1 +th1
+    +tr1 
+    +tr
+       +td+  i.d  +td1
+       +td+  i.d1 +td1
+    +tr1 
+    +tr
+       +td+  i.d2 +td1
+       +td+  i.d3 +td1
+    +tr1 
+    +tr
+       +td+  i.d4 +td1
+       +td+  i.d5 +td1
+    +tr1   
+    +tr
+       +td+  i.d6 +td1
+       +td+  i.d7 +td1
+    +tr1   
+    +tr
+       +td+  i.d8 +td1
+       +td+  i.d9 +td1
+    +tr1   
+    +tr
+       +td+  i.d10 +td1
+       +td+  i.d11 +td1
+    +tr1 
+    +tr
+       +td+  i.d12 +td1
+       +td+  i.d13 +td1
+    +tr1 
+  
+    +tr
+       +td+  i.d14 +td1
+       +td+  i.d15 +td1
+    +tr1 
+    +tr
+       +td+  i.d16 +td1
+       +td+  i.d17 +td1
+    +tr1  
+    +tr
+       +td+  i.d18 +td1
+       +td+  i.d19 +td1
+    +tr1 
+    +tr
+       +td+  i.d20 +td1
+       +td+  i.d21 +td1
+    +tr1   
+    +tr
+       +td+  i.d22 +td1
+       +td+  i.d23 +td1
+    +tr1   
+    +tr
+       +td+  i.d24 +td1
+       +td+  i.d25 +td1
+    +tr1 
+    +tr
+       +td+  i.d26 +td1
+       +td+  i.d27 +td1
+    +tr1 
+    +tr
+       +td+  i.d28 +td1
+       +td+  i.d29 +td1
+    +tr1   
+    +tr
+       +td+  i.d30 +td1
+       +td+  i.d31 +td1
+    +tr1   
+    +tr
+       +td+  i.d32 +td1
+       +td+  i.d33 +td1
+    +tr1   
+    +tr
+       +td+  i.d34 +td1
+       +td+  i.d35 +td1
+    +tr1 
+    +tr
+       +td+  i.d36 +td1
+       +td+  i.d37 +td1
+    +tr1 
+    +tr
+       +td+  i.d38 +td1
+       +td+  i.d39 +td1
+    +tr1   
+    +tr
+       +td+  i.d40 +td1
+       +td+  i.d41 +td1
+    +tr1        
+     +tr
+       +td+  i.d42 +td1
+       +td+  i.d43 +td1
+    +tr1   
+    +tr
+       +td+  i.d44 +td1
+       +td+  i.d45 +td1
+    +tr1 
+    +tr
+       +td+  i.d46 +td1
+       +td+  i.d47 +td1
+    +tr1 
+    +tr
+       +td+  i.d48 +td1
+       +td+  i.d49 +td1
+    +tr1   
+    +tr
+       +td+  i.d50 +td1
+       +td+  i.d51 +td1
+    +tr1         
+    +tr
+       +td+  i.d52 +td1
+       +td+  i.d53 +td1
+    +tr1   
+    +tr
+       +td+  i.d54 +td1
+       +td+  i.d55 +td1
+    +tr1 
+    +tr
+       +td+  i.d56 +td1
+       +td+  i.d57 +td1
+    +tr1 
+    +tr
+       +td+  i.d58 +td1
+       +td+  i.d59 +td1
+    +tr1   
+    +tr
+       +td+  i.d60 +td1
+       +td+  i.d61 +td1
+    +tr1          
+  
+    +tr
+       +td+  i.d62 +td1
+       +td+  i.d63 +td1
+    +tr1   
+    +tr
+       +td+  i.d64 +td1
+       +td+  i.d65 +td1
+    +tr1 
+    +tr
+       +td+  i.d66 +td1
+       +td+  i.d67 +td1
+    +tr1 
+    +tr
+       +td+  i.d68 +td1
+       +td+  i.d69 +td1
+    +tr1   
+    +tr
+       +td+  i.d70 +td1
+       +td+  i.d71 +td1
+    +tr1          
+    +tr
+       +td+  i.d72 +td1
+       +td+  i.d73 +td1
+    +tr1   
+    +tr
+       +td+  i.d74 +td1
+       +td+  i.d75 +td1
+    +tr1 
+    +tr
+       +td+  i.d76 +td1
+       +td+  i.d77 +td1
+    +tr1 
+    +tr
+       +td+  i.d78 +td1
+       +td+  i.d79 +td1
+    +tr1   
+    +tr
+       +td+  i.d80 +td1
+       +td+  i.d81 +td1
+    +tr1          
+    +tr
+       +td+  i.d82 +td1
+       +td+  i.d83 +td1
+    +tr1   
+    +tr
+       +td+  i.d84 +td1
+       +td+  i.d85 +td1
+    +tr1 
+    +tr
+       +td+  i.d86 +td1
+       +td+  i.d87 +td1
+    +tr1 
+    +tr
+       +td+  i.d88 +td1
+       +td+  i.d89 +td1
+    +tr1   
+    +tr
+       +td+  i.d90 +td1
+       +td+  i.d91 +td1
+    +tr1          
+    +tr
+       +td+  i.d92 +td1
+       +td+  i.d93 +td1
+    +tr1   
+    +tr
+       +td+  i.d94 +td1
+       +td+  i.d95 +td1
+    +tr1 
+    +tr
+       +td+  i.d96 +td1
+       +td+  i.d97 +td1
+    +tr1 
+    +tr
+       +td+  i.d98 +td1
+       +td+  i.d99 +td1
+    +tr1   
+    +tr
+       +td+  i.d100 +td1
+       +td+  i.d101 +td1
+    +tr1      
+    +tr
+       +td+  i.d102 +td1
+       +td+  i.d103 +td1
+    +tr1   
+    +tr
+       +td+  i.d104 +td1
+       +td+  i.d105 +td1
+    +tr1 
+    +tr
+       +td+  i.d106 +td1
+       +td+  i.d107 +td1
+    +tr1 
+    +tr
+       +td+  i.d108 +td1
+       +td+  i.d109 +td1
+    +tr1   
+    +tr
+       +td+  i.d110 +td1
+       +td+  i.d111 +td1
+    +tr1          
+  
+    +tr
+       +td+  i.d112 +td1
+       +td+  i.d113 +td1
+    +tr1   
+    +tr
+       +td+  i.d114 +td1
+       +td+  i.d115 +td1
+    +tr1 
+    +tr
+       +td+  i.d116 +td1
+       +td+  i.d117 +td1
+    +tr1 
+    +tr
+       +td+  i.d118 +td1
+       +td+  i.d119 +td1
+    +tr1   
+    +tr
+       +td+  i.d120 +td1
+       +td+  i.d121 +td1
+    +tr1            
+  
+  
+  
+  
+  
+// end   
++at1  
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
