1ca1468520c671f470abc51e160c500972533ee0
Miguel <welding3000@users.noreply.github.com>
2017-01-29 21:45:15 -0500

    Create structure-table.js

---
+++ Md-table/structure-table.js
@@ -0,0 +1,165 @@
+
+var t={
+// start  
+tb:'<table>',
+tr:'<tr>',
+td:'<td>',  
+th:'<th>',
+// end  
+endtb:'</table>',  
+endtr:'</tr>',
+endtd:'</td> ',  
+endth:'</th>',
+}
+// begin - end table
+var at= t.tb ;  var at1=t.endtb ;
+// begin - end th
+var th= t.th;   var th1= t.endth;
+// begin - end tr
+var tr = t.tr;  var tr1= t.endtr;
+// begin - end td
+var td = t.td;  var td1= t.endtd;
+
+document.write(
+// begin   
+at
+   +tr
+       +th+  i.h  +th1 
+       +th+  i.h1 +th1
+    +tr1 
+    +tr
+       +td+  i.d  +td1
+       +td+  i.d1 +td1
+    +tr1 
+    +tr
+       +td+  i.d2 +td1
+       +td+  i.d3 +td1
+    +tr1 
+    +tr
+       +td+  i.d4 +td1
+       +td+  i.d5 +td1
+    +tr1   
+    +tr
+       +td+  i.d6 +td1
+       +td+  i.d7 +td1
+    +tr1   
+    +tr
+       +td+  i.d8 +td1
+       +td+  i.d9 +td1
+    +tr1   
+    +tr
+       +td+  i.d10 +td1
+       +td+  i.d11 +td1
+    +tr1 
+    +tr
+       +td+  i.d12 +td1
+       +td+  i.d13 +td1
+    +tr1 
+  
+    +tr
+       +td+  i.d14 +td1
+       +td+  i.d15 +td1
+    +tr1 
+    +tr
+       +td+  i.d16 +td1
+       +td+  i.d17 +td1
+    +tr1  
+    +tr
+       +td+  i.d18 +td1
+       +td+  i.d19 +td1
+    +tr1 
+    +tr
+       +td+  i.d20 +td1
+       +td+  i.d21 +td1
+    +tr1   
+    +tr
+       +td+  i.d22 +td1
+       +td+  i.d23 +td1
+    +tr1   
+    +tr
+       +td+  i.d24 +td1
+       +td+  i.d25 +td1
+    +tr1 
+    +tr
+       +td+  i.d26 +td1
+       +td+  i.d27 +td1
+    +tr1 
+    +tr
+       +td+  i.d28 +td1
+       +td+  i.d29 +td1
+    +tr1   
+    +tr
+       +td+  i.d30 +td1
+       +td+  i.d31 +td1
+    +tr1   
+     
+// end   
++at1  
+);
+
+
+
+
+// -----------------------------------------------------------------
+
+
+<script>
+
+var i={
+
+h:'holaaaaaaaaaa',  
+h1:'hola',      
+  
+d  :'		',  
+d1 :'		',    
+d2 :'		',   
+d3 :'		', 
+d3 :'		',
+d4 :'		',
+d5 :'		',
+d6 :'		',
+d7 :'		',
+d8 :'		',
+d9 :'		',
+d10:'		',
+d11:'		',
+d12:'		',
+d13:'		',
+d14:'		',
+d15:'		',
+d16:'		',
+d17:'		',
+d18:'		',
+d19:'		',
+d20:'		',
+d21:'		',
+d22:'		',
+d23:'		',
+d24:'		',
+d25:'		',
+d26:'		',
+d27:'		',
+d28:'		',
+d29:'		',  
+d30:'		',  
+d31:'		',  
+  
+  
+  
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
